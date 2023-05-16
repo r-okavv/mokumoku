ca7ec046707fc7e9ae0115262feff6d05d274abd
r-okavv <ooxxbookxxoo@gmail.com>
2023-05-17 00:29:43 +0900

    mod: UserTableにgenderカラムを追加、サインアップフォームにgenderのセレクトボックスを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,9 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true
+  validates :gender, presence: true
+
+  enum gender: {man: 0, woman: 1}
 
   scope :allowing_created_event_notification,
         -> { joins(:notification_timings).merge(NotificationTiming.created_event) }
